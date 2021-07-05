8bf2cac500d2a2e79b8a5c98088dfe53c5e4a7b9
Patrik Valo <valo.patrik@gmail.com>
2021-07-06 00:06:07 +0200

    feat: add core functionality

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/PatrikValo/ssh-automation/cli"
+	"github.com/PatrikValo/ssh-automation/core"
+	"github.com/PatrikValo/ssh-automation/printer"
+	"github.com/PatrikValo/ssh-automation/program"
+	"os"
+)
+
+func main() {
+	userInput, err := cli.GetUserInput()
+
+	if err != nil {
+		panic(err)
+	}
+
+	parser := program.Parser{Filename: userInput.Filename}
+
+	parsedProgram, err := parser.Parse()
+	if err != nil {
+		panic(err)
+	}
+
+	user := userInput.Auth.User
+	auth, err := userInput.Auth.GetAuthMethod()
+	if err != nil {
+		panic(err)
+	}
+
+	terminal := make(chan printer.Printer)
+	executor := core.ExecutorFactory(parsedProgram.Hosts, user, &auth, terminal)
+
+	go func() {
+		defer close(terminal)
+		err := executor.Execute(parsedProgram)
+		if err != nil {
+			os.Exit(1)
+		}
+	}()
+
+	printer.RegisterPrinter(terminal)
+	os.Exit(0)
+}
