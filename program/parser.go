9830f032b5d0a8efdc3314c67732b07699ebb552
Patrik Valo <valo.patrik@gmail.com>
2021-07-30 12:18:24 +0200

    feat: add default port

--- program/parser.go
+++ program/parser.go
@@ -1,15 +1,36 @@
 package program
 
 import (
+	"errors"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"strings"
 )
 
 type Parser struct {
 	Filename string
 }
 
-func (parser Parser) Parse() (*Program, error) {
+func (parser *Parser) addDefaultPort(program *Program) error {
+	for i, host := range program.Hosts {
+		split := strings.Split(host, ":")
+
+		if len(split) > 2 {
+			return errors.New("bad format of host ip address or domain")
+		}
+
+		if len(split) == 2 && split[1] == "" {
+			program.Hosts[i] = host + "22"
+		}
+
+		if len(split) == 1 {
+			program.Hosts[i] = host + ":22"
+		}
+	}
+	return nil
+}
+
+func (parser *Parser) Parse() (*Program, error) {
 	yamlFile, err := ioutil.ReadFile(parser.Filename)
 
 	if err != nil {
@@ -18,6 +39,10 @@ func (parser Parser) Parse() (*Program, error) {
 
 	var program Program
 	err = yaml.Unmarshal(yamlFile, &program)
+
+	if err == nil {
+		err = parser.addDefaultPort(&program)
+	}
 
 	return &program, err
 }
